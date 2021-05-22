1aaaa596bb972f73e7f60203ffe04c37cabbbd8d
warhuhn <warhuhn@posteo.de>
2021-05-22 16:57:37 +0200

    Use autoloading for test.php, use __DIR__ constant for file paths

--- tests/test.php
+++ tests/test.php
@@ -1,12 +1,11 @@
 <?php
 
-require_once('../vendor/autoload.php');
-require_once('../src/class.dom-document.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
 
 use \PerryRylance\DOMDocument as DOMDocument;
 
 $document = new DOMDocument();
-$document->load("sample.html");
+$document->load(__DIR__ . "/sample.html");
 
 $tests = [
 	[
